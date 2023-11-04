c54b69f89b72cfde8f144f046e558da004c4f454
Mengyuan Li <mli114@volvocars.com>
2023-11-04 12:05:23 +0100

    updated page content

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,24 +1,23 @@
-import star from "../assets/star.png";
-import katie from "../assets/katie-zaferes.png";
+import star from "../../public/assets/star.png";
 
-export default function Card() {
+export default function Card(props) {
 	return (
 		<div className="card">
 			<img
 				className="card--image"
-				src={katie}
+				src={`../../public/assets/${props.img}`}
 				alt="Katie Zaferes"
 				width="176px"
 			/>
 			<div className="card--stats">
 				<img src={star} alt="5 stars" width="14px" height="14px" />
-				<span>5.0 </span>
-				<span className="gray">(6) • </span>
-				<span className="gray">USA</span>
+				<span>{props.rating} </span>
+				<span className="gray">({props.reviewCount}) • </span>
+				<span className="gray">{props.country}</span>
 			</div>
-			<p>Life lessons with Katie Zaferes</p>
+			<p>{props.title}</p>
 			<p>
-				<span className="bold">From $136</span> / person
+				<span className="bold">From ${props.price}</span> / person
 			</p>
 		</div>
 	);
